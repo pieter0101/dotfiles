7a5ede0ca06f8067c0d6593e1acadb56c2eaea1d
Pieter <pieter.kn0101@gmail.com>
2025-08-12 20:55:28 +0200

    neovim: added tombi as toml lsp

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -26,6 +26,7 @@ return {
             qmlls = 'qmlls6',
             nixd = 'nixd',
             pyright = 'pyright',
+            tombi = 'tombi',
             -- yamlls = "yaml-language-server",
         }
 

89b0033a71355e087722b036951650b7df079fb3
Pieter <pieter.kn0101@gmail.com>
2025-08-26 21:37:14 +0200

    nix: make AFL++ linux only as nixpkgs doesn't support it on darwin

--- home/.config/nix/modules/development.nix
+++ home/.config/nix/modules/development.nix
@@ -1,22 +1,26 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    aflplusplus
-    bash-language-server
-    clang
-    clang-tools
-    gcc
-    gdb
-    ghidra
-    lua-language-server
-    nix-output-monitor
-    nixd
-    nixfmt-rfc-style
-    pyright
-    rustup
-    shellcheck
-    shfmt
-    stylua
-    tombi
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      bash-language-server
+      clang
+      clang-tools
+      gcc
+      gdb
+      ghidra
+      lua-language-server
+      nix-output-monitor
+      nixd
+      nixfmt-rfc-style
+      pyright
+      rustup
+      shellcheck
+      shfmt
+      stylua
+      tombi
+    ]
+    ++ (lib.optionals (pkgs.system == "x86_64-linux") [
+      aflplusplus
+    ]);
 }
